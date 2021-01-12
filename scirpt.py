5ef57f506550c88da10788045fc7877ddd5d1763
Eros Filippi <eros.filippi@sacertis.com>
2021-01-12 17:44:20 +0100

    Update scirpt.py

--- scirpt.py
+++ scirpt.py
@@ -2,14 +2,63 @@
 
 print("esempio di codice in produzione")
 
-################################################################
+################################# PAOLA
+print("line1")
+print("line2")
+print("line3")
 
-print("prova1")
-print("prova2")
-print("Modifica EF1")
+################################# ISA
+print("line1")
+print("line2")
+print("line3")
 
-########################################################
+################################# MONICA
+print("line1")
+print("line2")
+print("line3")
 
-print("prova1")
-print("modifica EF2")
-print("prova3")
+################################# BENITO
+print("line1")
+print("line2")
+print("line3")
+
+################################# DARIO
+print("line1")
+print("line2")
+print("line3")
+
+################################# FRANCESCO
+print("line1")
+print("line2")
+print("line3")
+
+################################# ELENA
+print("line1")
+print("line2")
+print("line3")
+
+################################# SARA
+print("line1")
+print("line2")
+print("line3")
+
+################################# LUDOVICO
+print("line1")
+print("line2")
+print("line3")
+
+################################# SALVATORE
+print("line1")
+print("line2")
+print("line3")
+
+################################# ALESSIO
+print("line1")
+print("line2")
+print("line3")
+
+################################# GIULIA
+print("line1")
+print("line2")
+print("line3")
+
